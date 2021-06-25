d3b50bd22e7873ce511ec64a2b0dc7b731f4fc5c
limy <mingyu6@staff.sina.com.cn>
2021-06-25 17:43:03 +0800

    fix: 删除无用文件，增加测试

--- src/views/index/App.vue
+++ src/views/index/App.vue
@@ -1,32 +1,35 @@
 <template>
   <!-- <section class="labour_card"> -->
-    <!-- <section class="aio_listen"> -->
-      <div class="aio_listen_feed">
-        <AioAudioBookItem
-
-          title="测试的"
-          intro="234 "
-          img-src="http://p8.sinaimg.cn/1093935975/180/1"
-          show-number="102w"
-          @item-mounted="itemMounted"
-        />
-      </div>
-    <!-- </section> -->
+  <!-- <section class="aio_listen"> -->
+  <div class="aio_listen_feed">
+    <AioAudioBookItem
+      title="测试的"
+      intro="234 "
+      img-src="http://p8.sinaimg.cn/1093935975/180/1"
+      show-number="102w"
+      @item-mounted="itemMounted"
+    />
+  <SnackBarTest />
+  </div>
+  <!-- </section> -->
   <!-- </section> -->
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component, Prop } from 'vue-property-decorator';
 import { AioAudioBookItem } from '~/index';
+import SnackBarTest from './components/SnackBar.vue'
 @Component({
   components: {
-    AioAudioBookItem
+    AioAudioBookItem,
+    SnackBarTest
   }
 })
 export default class App extends Vue {
-  itemMounted(){
-    console.log('itemMounted')
+  itemMounted() {
+    console.log('itemMounted');
   }
+
 }
 </script>
 
---
+++ src/views/index/components/SnackBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>snackbar 测试</p>
+    <button @click="isShowSnackbar = !isShowSnackbar">{{ btnText }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch } from 'vue-property-decorator';
+import SnackBar, { IOptions } from '../../../../packages/snack-bar';
+
+@Component({
+  components: {}
+})
+export default class SnackBarTest extends Vue {
+  isShowSnackbar: boolean = false;
+
+  get btnText(): string {
+    return this.isShowSnackbar ? '收起' : '展示';
+  }
+
+  @Watch('isShowSnackbar')
+  onChange(val) {
+    if (val) {
+      SnackBar.show(<IOptions>{
+        content: '测试的content',
+        btnText: '关注',
+        icon:
+          'https://img2.baidu.com/it/u=1954131356,3549453457&fm=26&fmt=auto&gp=0.jpg'
+      });
+    } else {
+      SnackBar.close();
+    }
+  }
+  mounted() {}
+}
+</script>
+
+<style scoped></style>
