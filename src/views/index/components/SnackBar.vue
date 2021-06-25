23096357fc12e0eea0f79dc5531af6ef8ecc3f82
limy <mingyu6@staff.sina.com.cn>
2021-06-25 19:46:07 +0800

    test: 测试

--- src/views/index/components/SnackBar.vue
+++ src/views/index/components/SnackBar.vue
@@ -1,39 +1,47 @@
 <template>
   <div>
     <p>snackbar 测试</p>
-    <button @click="isShowSnackbar = !isShowSnackbar">{{ btnText }}</button>
+    <button @click="show">展示</button>
+    <button @click="hide">隐藏</button>
+    <button @click="check">检查是否存在</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Watch } from 'vue-property-decorator';
-import SnackBar, { IOptions } from '../../../../packages/snack-bar';
+import { Vue, Component } from 'vue-property-decorator';
+import SnackBar, { IOptions } from '@mfelibs/aio-snack-bar';
 
 @Component({
   components: {}
 })
 export default class SnackBarTest extends Vue {
-  isShowSnackbar: boolean = false;
-
-  get btnText(): string {
-    return this.isShowSnackbar ? '收起' : '展示';
+  show() {
+    SnackBar.show(<IOptions>{
+      content: '测试的content',
+      btnText: '关注',
+      icon:
+        'https://img2.baidu.com/it/u=1954131356,3549453457&fm=26&fmt=auto&gp=0.jpg',
+      distance: 200,
+      duration: 2000,
+      transitionDuration: 500,
+      iconBorderRadius: '10px',
+      onClickButton: () => {
+        console.log(`[SnackBar] onClickButton cb`);
+      },
+      onClose: () => {
+        console.log(`[SnackBar] onClose cb`);
+      },
+      onUserClickClose: () => {
+        console.log(`[SnackBar] onUserClickClose cb`);
+      }
+    });
+  }
+  hide() {
+    SnackBar.close();
   }
 
-  @Watch('isShowSnackbar')
-  onChange(val) {
-    if (val) {
-      SnackBar.show(<IOptions>{
-        content: '测试的content',
-        btnText: '关注',
-        icon:
-          'https://img2.baidu.com/it/u=1954131356,3549453457&fm=26&fmt=auto&gp=0.jpg'
-      });
-    } else {
-      SnackBar.close();
-    }
+  check() {
+    console.log(`[SnackBar] `, SnackBar.checkExist());
   }
-  mounted() {}
 }
 </script>
-
-<style scoped></style>
