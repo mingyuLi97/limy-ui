b387aed3872f274059d0867cdd344c6aae619b6a
mingyu6 <mingyu6@staff.sina.com.cn>
2021-11-24 15:46:47 +0800

    feat: layout

---
+++ src/components/layout/LayoutItem.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import { CreateElement } from 'vue';
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { createBEM } from '~/utils/create/bem';
+
+@Component({
+  components: {}
+})
+export default class LayoutItem extends Vue {
+  /**
+   * 渲染的标签名
+   */
+  @Prop({ default: 'div' }) tag!: keyof HTMLElementTagNameMap;
+
+  @Prop({ required: true, type: Number }) span: number;
+
+  @Prop({ required: false, type: Number, default: 0 }) offset: number;
+
+  get b() {
+    return createBEM('limy-layout');
+  }
+  get classes() {
+    const { b, span, offset } = this;
+    return [
+      b('col', [
+        {
+          [span]: span,
+          ['offset-' + offset]: offset
+        }
+      ])
+    ];
+  }
+
+  render(h: CreateElement) {
+    return h(
+      this.tag,
+      {
+        class: `${this.classes}`
+      },
+      this.$slots.default
+    );
+  }
+}
+</script>
+
+<style scoped></style>
